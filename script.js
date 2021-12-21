eada4f3c6b54f3581cffc601fc1e6b7d0179d713
soutik2409 <83855585+soutik2409@users.noreply.github.com>
2021-12-21 17:00:11 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready( () => {
-    $('#Studentdetails').click( () => {
-        console.log("kli");
-        $.getJSON("../api/students/"+$('#roll').val(), (result,status) => {
-            console.log(result);
-        });
-    });
-});
